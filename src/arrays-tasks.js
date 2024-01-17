4d8dd8af00ca756332cf221a275055dcfcc95704
AndriiHordov <146883659+AndriiHordov@users.noreply.github.com>
2024-01-17 01:17:25 +0100

    feat: implement shiftArray function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -538,8 +538,13 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const copy = [...arr];
+  const result = copy.splice(
+    n > 0 ? n + 1 : Math.abs(n),
+    copy.length - Math.abs(n)
+  );
+  return result.concat(copy);
 }
 
 /**
