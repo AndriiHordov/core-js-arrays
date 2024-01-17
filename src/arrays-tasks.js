2e075acbeb58d548834e6acc146feade52a22127
AndriiHordov <146883659+AndriiHordov@users.noreply.github.com>
2024-01-17 23:49:09 +0100

    feat: implement generateOdds function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -358,9 +358,7 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
-}
+const generateOdds = (len) => new Array(len).fill(1).map((el, i) => el + i * 2);
 
 /**
  * Returns an element from the multidimensional array by the specified indices.
