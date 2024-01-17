4de948c12dec4672bbabfe0cb4cba45f7773b343
AndriiHordov <146883659+AndriiHordov@users.noreply.github.com>
2024-01-17 01:16:18 +0100

    feat: implement findCommonElements function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -483,8 +483,13 @@ function getMaxItems(/* arr, n */) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const source = arr1.concat(arr2);
+  return [
+    ...new Set(
+      source.filter((el) => source.indexOf(el) !== source.lastIndexOf(el))
+    ),
+  ];
 }
 
 /**
